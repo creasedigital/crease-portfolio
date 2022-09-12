de34e2e956de6fa4baf458d12094f239bff9b6e4
creasedigital <ojorechris@gmail.com>
2022-09-12 15:22:17 +0100

    views pages created

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import ColorToggler from "../components/ColorToggler";
+import ColorToggler from "../../components/ColorToggler";
 
 const Home = () => {
 	return (
