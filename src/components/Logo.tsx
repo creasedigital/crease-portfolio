3c645177b8970fb7c4e2eec14d66889930466b33
creasedigital <ojorechris@gmail.com>
2022-09-12 16:02:46 +0100

    logo props typing

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,19 @@
-import {Box, Text} from '@chakra-ui/react'
+import { Box, ChakraProps, OmitCommonProps, Text } from "@chakra-ui/react";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
-export default function Logo() {
-  return (
-    <Box>    
-        <Text
-        fontSize="lg" fontWeight={"bold"}
-        >
-            Ojore_Chris
-        </Text>
-    </Box>
-  )
+export default function Logo(
+	props: JSX.IntrinsicAttributes &
+		OmitCommonProps<
+			DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+			keyof ChakraProps
+		> &
+		ChakraProps & { as?: "div" | undefined },
+) {
+	return (
+		<Box {...props}>
+			<Text fontSize="lg" fontWeight={"bold"}>
+				Ojore_Chris
+			</Text>
+		</Box>
+	);
 }
