a1113602b16e05d6deaa7c216de2a4570b08d059
creasedigital <ojorechris@gmail.com>
2022-09-12 15:29:20 +0100

    placeholder logo added

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import {Box, Text} from '@chakra-ui/react'
+
+export default function Logo() {
+  return (
+    <Box>    
+        <Text
+        fontSize="lg" fontWeight={"bold"}
+        >
+            Ojore_Chris
+        </Text>
+    </Box>
+  )
+}
