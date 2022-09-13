cc944c3fd8078bd2ce76b4802c9d8c6020c2d1a9
creasedigital <ojorechris@gmail.com>
2022-09-13 22:12:32 +0100

    about view text

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,4 +1,22 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
 const About = () => {
-	return <div>About</div>;
+	return (
+		<Box>
+			<Heading>
+				I will tell you an open secret <Box as="span">about me.</Box>I
+				am all about <Box as="span">LOVE</Box>
+			</Heading>
+			<Box></Box>
+			<Heading>
+				What do you <Box as="span">LOVE?</Box>
+			</Heading>
+			<Box>
+				<Text>Send me a mail</Text>
+				<Text>ojorechris@gmail.com</Text>
+				<Text>I'd love to work with you!</Text>
+			</Box>
+		</Box>
+	);
 };
 export default About;
