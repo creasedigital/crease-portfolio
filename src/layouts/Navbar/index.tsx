ab8fbe080d8528a28daeb99aa619501599e7e914
creasedigital <ojorechris@gmail.com>
2022-09-13 06:56:53 +0100

    vite path config

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,4 +1,19 @@
-const Navbar = () => {
-	return <div>Navbar</div>;
+import Logo from "components/Logo";
+import { useState } from "react";
+import MenuLinks from "../MenuLinks";
+import MenuToggle from "../MenuToggle";
+import NavBarContainer from "../NavBarContainer";
+
+const NavBar = (props: any) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<NavBarContainer {...props}>
+			<Logo w="100px" />
+			<MenuToggle toggle={toggle} isOpen={isOpen} />
+			<MenuLinks isOpen={isOpen} />
+		</NavBarContainer>
+	);
 };
-export default Navbar;
+export default NavBar;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
+import Navbar from "layouts/Navbar";
 import ColorToggler from "../../components/ColorToggler";
 
 const Home = () => {
 	return (
 		<Box minHeight={"100vh"}>
 			<ColorToggler />
+			<Navbar />
 			Home
 		</Box>
 	);
