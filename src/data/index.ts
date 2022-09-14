1f7a4a199cddeabe8f6de451e4783cead7c4208e
creasedigital <ojorechris@gmail.com>
2022-09-14 08:00:57 +0100

    projects array updated

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
-import CREASE from "assets/imgs/crease.jpg"
+// import CREASE from "assets/imgs/crease.jpg"
 import MAINSTACK from "assets/imgs/1_mainstack.jpg"
 import FIXAM from "assets/imgs/2_fixam.jpg"
 import UNBOXD from "assets/imgs/3_unboxd.jpg"
@@ -35,7 +35,7 @@ interface ILiveProjects{
 const sideProjects: ISideProjects[] = [
     {
         id: 1,
-        img: CREASE,
+        img: ADROSOL,
         title: "adrosol",
         desc: "This project was created using the free json placeholder api and displaying a list of users that can be filtered with search strings for name or username",
         stack: ['react', 'axios', 'chakra-ui' ],
@@ -43,7 +43,7 @@ const sideProjects: ISideProjects[] = [
     },
     {
         id: 2,
-        img: CREASE,
+        img: DASHBOARD,
         title: "dashboard",
         desc: "I was inspired to build a simple dashboard application from a design I saw online to illustrate some graphical data which is applicable to business cases.",
         stack: ['react', 'rechart', 'typescript', 'redux-toolkit', 'chakra-ui' ],
@@ -51,7 +51,7 @@ const sideProjects: ISideProjects[] = [
     },
     {
         id: 3,
-        img: CREASE,
+        img: CLOUDBANK,
         title: "cloud bank",
         desc: "Achieving a pixel-perfect design is often the minimum requirement expected of a frontend developer. In this project, I reproduced a pixel-perfect version of the cloud bank 'loan page' design.",
         stack: ['react', 'react-router-dom', 'tailwind-css' ],
@@ -59,7 +59,7 @@ const sideProjects: ISideProjects[] = [
     },
     {
         id: 4,
-        img: CREASE,
+        img: GITHUBUSER,
         title: "github profile finder",
         desc: "This app was created to search github user profile information and display a few details",
         stack: ['react', 'react-router-dom', 'react-bootstrap', 'github-api' ],
@@ -71,7 +71,7 @@ const sideProjects: ISideProjects[] = [
 const liveProjects: ILiveProjects[] = [
     {
         id: 41,
-        img: CREASE,
+        img: MAINSTACK,
         title: "mainstack",
         desc: "Having ran a promising beta version of this startup social media app that has amassed over six thousand us across 32+ countries globally, Mainstack decided to upgrade her product with several new features and entirely rewrote her codebase to include a bunch of features which will help developers, creatives and a wide range of entrepreneurs monetize their platforms.",
         role: "I worked on important features and pages across the app which includes: Dashboard, Digital Shop, Masterclass, Tribe, Pricing, Help Center, Report A Bug , Suggest A feature, Integrating API, amongst other tasks.",
@@ -80,7 +80,7 @@ const liveProjects: ILiveProjects[] = [
     },
     {
         id: 42,
-        img: CREASE,
+        img: FIXAM,
         title: "fixam backoffice",
         desc: "How many times have you had a need of getting a technician near you but struggled? Fixam Backoffice was designed to solve this challenge and I was privileged to build the web app version of this product from the scratch that will prospectively create thousands of opportunity for technicians and serve millions of users as it grows. I am eagerly waiting for the launch of this product by the first quarter of the comming year.",
         role: "I built this app from the start with a few collaboration towards its closing stages of development.",
@@ -89,7 +89,7 @@ const liveProjects: ILiveProjects[] = [
     },
     {
         id: 43,
-        img: CREASE,
+        img: UNBOXD,
         title: "unboxd",
         desc: "Have you ever wished you could acquire something you desired but never could? Yes! You probably couldn't because you didn't know how to source for funds. Welcome to Unboxd!! This solution will help you cut cost on wasteful spending through creating  a wishlist, and your wishes come true.",
         role: "I managed and maintained the codebase and made changes to the app based on customers' needs and organization's requirements.",
@@ -98,7 +98,7 @@ const liveProjects: ILiveProjects[] = [
     },
     {
         id: 44,
-        img: CREASE,
+        img: GACA,
         title: "greenpeg academy",
         desc: "GACA platform offers professional training services to young engineers to bridge skill gaps with courses tailored to suit learning objectives based on learner's needs.",
         role: "During my earlier days as a web developer, I was a part of the team that worked on this technical training platform. I worked on styling and reponsive implementations. ",
